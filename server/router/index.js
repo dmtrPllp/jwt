984b0b7f24800cb994f63cdadf5cf8de30382085
Dmitry <dmitry.p00.02@mail.ru>
2022-10-29 22:59:19 +0300

    reg validation

--- server/router/index.js
+++ server/router/index.js
@@ -1,8 +1,14 @@
 const Router = require('express').Router;
-const userController= require('../controllers/user-controller');
+const userController = require('../controllers/user-controller');
 const router = new Router();
+const { body } = require('express-validator');
 
-router.post('/registration', userController.registration);
+router.post('/registration',
+    body('email').isEmail(),
+    body('password','Password should be combination of one uppercase , one lower case, one special char, one digit and min 8 , max 20 char long')
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^a-zA-Z0-9])\S{1,16}$/),
+    userController.registration
+);
 router.post('/login', userController.login);
 router.post('/logout', userController.logout);
 router.get('/activate/:link', userController.activate);
