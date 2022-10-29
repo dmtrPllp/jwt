9e68456ee092c0be27005366101ca0d944b87fe6
Dmitry <dmitry.p00.02@mail.ru>
2022-10-29 18:39:19 +0300

    end UserService.registration

--- server/services/user-service.js
+++ server/services/user-service.js
@@ -2,6 +2,7 @@ const UserModel = require('../models/user-model');
 const bcrypt = require('bcrypt');
 const tokenService = require('../services/token-service');
 const mailService = require('../services/mail-service');
+const UserDto = require('../dto/user-dto');
 
 
 class UserService {
@@ -14,8 +15,14 @@ class UserService {
         const activationLink = uuid.v4();
         const user = await UserModel.create({ email, password: hashpassword })
         await mailService.sendActivationMail(email, activationLink);
-        const token = tokenService.generateTokens()
-
+        
+        const userDto = new UserDto(user);
+        const tokens = tokenService.generateTokens({...userDto});
+        await tokenService.saveToken(userDto.id, tokens.refreshToken);
+        return {
+            ...tokens,
+            user: userDto
+        }
     }
 }
 
